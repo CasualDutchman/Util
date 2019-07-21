4ff5f39c424fd355a1ec960d1a58bdc867424b54
Casual Dutchman <pieterj@live.nl>
2019-07-22 01:32:03 +0200

    Float Extension + Vector2 clamp

---
+++ Extensions/FloatExtension.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FloatExtension 
+{
+	public static float O1(this float f)
+		=> Mathf.Clamp01(f);
+}
--- Extensions/Vector2Extension.cs
+++ Extensions/Vector2Extension.cs
@@ -8,5 +8,8 @@ namespace Framework
 	{
 		public static Vector3 XOY(this Vector2 v2)
 			=> new Vector3(v2.x, 0, v2.y);
+
+		public static Vector2 Clamp(this Vector2 v2, Vector2 min, Vector2 max)
+			=> new Vector2(Mathf.Clamp(v2.x, min.x, max.x), Mathf.Clamp(v2.y, min.y, max.y));
 	}
 }
