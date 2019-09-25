bf1bca75a9319a20fb026666e2e11cb5960324d7
Casual Dutchman <pieterj@live.nl>
2019-09-25 23:51:44 +0200

    ToVector3

--- Extensions/Vector2Extension.cs
+++ Extensions/Vector2Extension.cs
@@ -18,6 +18,9 @@ namespace Framework
 		public static Vector2 Clamp(this Vector2 v2, Vector2 min, Vector2 max)
 			=> new Vector2(Mathf.Clamp(v2.x, min.x, max.x), Mathf.Clamp(v2.y, min.y, max.y));
 
+		public static Vector3 ToVector3(this Vector2 v2, float z = 0)
+			=> new Vector3(v2.x, v2.y, z);
+
 		public static Vector2Int ToVector2Int(this Vector2 v2)
 			=> new Vector2Int((int)v2.x, (int)v2.y);
 
