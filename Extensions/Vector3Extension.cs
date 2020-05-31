5c732baff01211444fae8a64b0a3fb9c7d5c5c7d
Casual Dutchman <pieterj@live.nl>
2020-05-31 13:21:00 +0200

    Vector3 Prediv, Mul

--- Extensions/Vector3Extension.cs
+++ Extensions/Vector3Extension.cs
@@ -23,5 +23,11 @@ namespace Framework
 
 		public static Vector3 SetZ(this Vector3 v3, float value)
 			=> new Vector3(v3.x, v3.y, value);
+
+		public static Vector3 PreDiv(this Vector3 v3, float value)
+			=> new Vector3(value / v3.x, value / v3.y, value / v3.z);
+
+		public static Vector3 Mul(this Vector3 v3a, Vector3 v3b)
+			=> new Vector3(v3a.x * v3b.x, v3a.y * v3b.y, v3a.z * v3b.z);
 	}
 }
