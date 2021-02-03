6341d60b3747f8c9eb2dcfc889b4e71a6d7e3b4e
Casual Dutchman <pieterj@live.nl>
2021-02-03 22:03:56 +0100

    Vector4 extension

---
+++ Extensions/Vector4Extension.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+	public static class Vector4Extension
+	{
+		public static Vector4 XYZO(this Vector3 v3, float f = 0)
+			=> new Vector4(v3.x, v3.y, v3.z, f);
+	}
+}
